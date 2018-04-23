259feb95017338193aebc6d273c5866efa7226ea
onsubat <m.b.altintas@hotmail.com>
2018-04-23 18:45:06 +0300

    location

--- app/src/main/java/com/come335/burak/erdem/yemeklik/MainActivity.java
+++ app/src/main/java/com/come335/burak/erdem/yemeklik/MainActivity.java
@@ -1,25 +1,35 @@
 package com.come335.burak.erdem.yemeklik;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnWhatToEat;
     Button btnShowMenu;
 
+    private static final String TAG = "MainActivity";
+    private static final int    ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+    if(isServicesOK()){
+        init();
+    }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
                                     /*Initializing Variables*/
 
@@ -46,5 +56,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void init()
+    {
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view)
+        {
+            Intent intent = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(intent);
+        }
+        });
+    }
+
+    public boolean isServicesOK()
+    {
+        Log.d(TAG, "isServicesOK: Checking Google Services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if (available== ConnectionResult.SUCCESS)
+        {
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
+        {
+            Log.d(TAG, "isServicesOK: An error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this , available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else
+            {
+                Toast.makeText(this,"We can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/come335/burak/erdem/yemeklik/MapActivity.java
@@ -0,0 +1,14 @@
+package com.come335.burak.erdem.yemeklik;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+    private static final String TAG = "MapActivity";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
