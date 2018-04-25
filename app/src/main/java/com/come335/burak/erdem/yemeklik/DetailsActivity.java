aeac48be07e61d3a1652b0561d18c338f0dd06c3
onsubat <m.b.altintas@hotmail.com>
2018-04-25 22:52:53 +0300

    LOKASYOOOOOOON

--- app/src/main/java/com/come335/burak/erdem/yemeklik/DetailsActivity.java
+++ app/src/main/java/com/come335/burak/erdem/yemeklik/DetailsActivity.java
@@ -1,9 +1,11 @@
 package com.come335.burak.erdem.yemeklik;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,6 +22,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -60,11 +64,18 @@ public class DetailsActivity extends AppCompatActivity {
     ImageView imageV;
     TextView ratingT;
 
+    private static final String TAG = "DetailsActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meal_details);
+
+        if(isServicesOK()){
+            init();
+        }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -189,6 +200,42 @@ public class DetailsActivity extends AppCompatActivity {
     private void getIdWithIntent()
     {
         mealId = getIntent().getIntExtra("id", 0);
+    }
+
+    private void init()
+    {
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(DetailsActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK()
+    {
+        Log.d(TAG, "isServicesOK: Checking Google Services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(DetailsActivity.this);
+        if (available== ConnectionResult.SUCCESS)
+        {
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
+        {
+            Log.d(TAG, "isServicesOK: An error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(DetailsActivity.this , available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else
+        {
+            Toast.makeText(this,"We can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
     private void setDetails(boolean _bool)
