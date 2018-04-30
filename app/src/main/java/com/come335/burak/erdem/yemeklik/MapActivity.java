1d87ff96d497164126d036835e568f1a4e0cc419
onsubat <m.b.altintas@hotmail.com>
2018-04-30 17:54:02 +0300

    Lokasyona Arama çubuğu eklendi ve girdileri algılıyor.

--- app/src/main/java/com/come335/burak/erdem/yemeklik/MapActivity.java
+++ app/src/main/java/com/come335/burak/erdem/yemeklik/MapActivity.java
@@ -14,8 +14,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +29,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -34,7 +38,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by User on 10/2/2017.
+ * Created by Burak on 25/4/2018.
  */
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -68,6 +72,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     //widgets
     private EditText mSearchText;
+    private ImageView mGps;
 
     //variables
     private Boolean mLocationPermissionsGranted = false;
@@ -79,6 +84,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         mSearchText = (EditText) findViewById(R.id.input_search);
+        mGps = (ImageView) findViewById(R.id.ic_gps);
 
         getLocationPermission();
     }
@@ -99,6 +105,14 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 return false;
             }
         });
+        mGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: cliked gps icon");
+                getDeviceLocation();
+            }
+        });
+        hideSoftKeyboard();
     }
     private void geoLocate() {
         Log.d(TAG, "geoLocate: geolocating");
@@ -114,7 +128,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         if (list.size()>0){
             Address address = list.get(0);
             Log.d(TAG, "geoLocate: found a location" + address.toString());
-            Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+            moveCamera(new LatLng(address.getLatitude(),address.getLongitude()), DEFAULT_ZOOM, address.getAddressLine(0));
 
         }
     }
@@ -135,7 +150,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                             Location currentLocation = (Location) task.getResult();
 
                             moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
-                                    DEFAULT_ZOOM);
+                                    DEFAULT_ZOOM, "My Location");
 
                         }else{
                             Log.d(TAG, "onComplete: current location is null");
@@ -149,9 +164,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
     }
 
-    private void moveCamera(LatLng latLng, float zoom){
-        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+    private void moveCamera(LatLng latLng, float zoom, String title) {
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        if (!title.equals("My Location")) {
+            MarkerOptions options = new MarkerOptions().position(latLng).title(title);
+            mMap.addMarker(options);
+        }
+        hideSoftKeyboard();
     }
 
     private void initMap(){
@@ -206,5 +227,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 }
             }
         }
+    }
+    private void hideSoftKeyboard() {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
